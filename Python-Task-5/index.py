db0037e330bb0ac681886705d0f6fc8215f868cf
Manish Kumar <65070286+manishkumar186@users.noreply.github.com>
2020-09-02 13:58:21 +0530

    Add files via upload

---
+++ Python-Task-5/index.py
@@ -0,0 +1,12 @@
+m = ["9887878787","aman@gmail.com","Aman", "hack@sachtechsolution.com", "98776", "abcd@yahoo.in"]
+result = filter(lambda x: "@" in x, m);
+print(list(result));
+
+
+name = ["Peter", "Aman", "Jerry", "James", "Amninder", "ER Harry Potter"];
+res = filter(lambda x:"er" in x,name)
+print(list(res));
+
+
+
+
